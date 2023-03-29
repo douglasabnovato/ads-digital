32c750df12265601c051d1336a00d71468cc2edb
douglasabnovato <douglasabnovato@gmail.com>
2023-03-29 18:27:35 -0300

    terminado código 4

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-//2 ) Fibonacci
+/** 
+ 
+ //4 ) O caminhão estará mais próximo da cidade de Ribeirão Preto devido a velocidade  de cada um dos veículos.
+ 
+*/
+
+/** 
+ //3 ) Lógica
+ a) 1, 3, 5, 7, _9_
+
+ b) 2, 4, 8, 16, 32, 64, _128_
+ 
+ c) 0, 1, 4, 9, 16, 25, 36, _49_
+ 
+ d) 4, 16, 36, 64, _100_
+ 
+ e) 1, 1, 2, 3, 5, 8, _13_
+ 
+ f) 2, 10, 12, 16, 17, 18, 19, _200_ : Todos os números começam com a letra D
+*/
+
+/** 
+ //2 ) Fibonacci
 console.log("executando Fibonacci...");
 
 let buscado = prompt("Informe um numero para pesquisar ");
@@ -41,6 +63,7 @@ if (encontrado == "sim") {
   console.log("O número informado ", buscado, " NÃO pertence a sequência.");
 }
  
+*/
 
 /** 
 //1 ) soma
@@ -56,4 +79,6 @@ while (k < indice) {
 }
 
 console.log("soma: ", soma);
+
+//resposta: soma = 91
 */
