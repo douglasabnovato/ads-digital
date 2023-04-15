1a9ce381a2f01162bb77b177cf2eb072d30226da
douglasabnovato <douglasabnovato@gmail.com>
2023-04-15 19:26:29 -0300

    funcionalidades

--- js-exercises/codigos/script.js
+++ js-exercises/codigos/script.js
@@ -100,13 +100,13 @@ function adicionarItem() {
 console.log("Calculadora ...");
 
 const calculate = (n1, operator, n2) => {
-  const firstNum = parseFloat(n1)
-  const secondNum = parseFloat(n2)
-  if (operator === 'add') return firstNum + secondNum
-  if (operator === 'subtract') return firstNum - secondNum
-  if (operator === 'multiply') return firstNum * secondNum
-  if (operator === 'divide') return firstNum / secondNum
-}
+  const firstNum = parseFloat(n1);
+  const secondNum = parseFloat(n2);
+  if (operator === "add") return firstNum + secondNum;
+  if (operator === "subtract") return firstNum - secondNum;
+  if (operator === "multiply") return firstNum * secondNum;
+  if (operator === "divide") return firstNum / secondNum;
+};
 
 const display = document.querySelector(".calculator__display");
 const calculator = document.querySelector(".calculator");
