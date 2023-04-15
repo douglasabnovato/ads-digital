bd1e6b90c4de7fb1c8f6cb407d1428b8da98ce84
douglasabnovato <douglasabnovato@gmail.com>
2023-04-15 11:25:40 -0300

    estilização das cores

--- js-exercises/codigos/script.js
+++ js-exercises/codigos/script.js
@@ -99,14 +99,21 @@ function adicionarItem() {
 
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector('.calculator__display');
 
 keys.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
 
     if (!action) {
-      console.log("number_key");
+      if (displayedNum === '0') {
+        display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent
+      }
     }
 
     if (
@@ -115,11 +122,11 @@ keys.addEventListener("click", (e) => {
       action === "multiply" ||
       action === "divide"
     ) {
-      console.log("operator key!");
+      key.classList.add('is-depressed')
     }
 
-    if (action === "decimal") {
-      console.log("decimal key!");
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.'
     }
 
     if (action === "clear") {
