e83207bdb92a61997dd7917856ea9437bcd7a2bd
douglasabnovato <douglasabnovato@gmail.com>
2023-04-15 18:04:21 -0300

    caminho de uso ideal para a calculadora

--- js-exercises/codigos/script.js
+++ js-exercises/codigos/script.js
@@ -97,6 +97,8 @@ function adicionarItem() {
   }
 }
 
+console.log("Calculadora ...");
+
 const calculate = (n1, operator, n2) => {
   let result = "";
 
@@ -113,9 +115,9 @@ const calculate = (n1, operator, n2) => {
   return result;
 };
 
+const display = document.querySelector(".calculator__display");
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
-const display = document.querySelector(".calculator__display");
 
 keys.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
@@ -130,66 +132,11 @@ keys.addEventListener("click", (e) => {
     );
 
     if (!action) {
-      if (
-        displayedNum === "0" ||
-        previousKeyType === "operator" ||
-        previousKeyType === "calculate"
-      ) {
+      if (displayedNum === "0" || previousKeyType === "operator") {
         display.textContent = keyContent;
       } else {
         display.textContent = displayedNum + keyContent;
       }
-      calculator.dataset.previousKeyType = "number";
-    }
-
-    if (action === "decimal") {
-      if (!displayedNum.includes(".")) {
-        display.textContent = displayedNum + ".";
-      } else if (
-        previousKeyType === "operator" ||
-        previousKeyType === "calculate"
-      ) {
-        display.textContent = "0.";
-      }
-
-      calculator.dataset.previousKeyType = "decimal";
-    }
-
-    if (action !== "clear") {
-      const clearButton = calculator.querySelector("[data-action=clear]");
-      clearButton.textContent = "CE";
-    }
-
-    if (action === "clear") {
-      if (key.textContent === "AC") {
-        calculator.dataset.firstValue = "";
-        calculator.dataset.modValue = "";
-        calculator.dataset.operator = "";
-        calculator.dataset.previousKeyType = "";
-      } else {
-        key.textContent = "AC";
-      }
-
-      display.textContent = 0;
-      calculator.dataset.previousKeyType = "clear";
-    }
-
-    if (action === "calculate") {
-      let firstValue = calculator.dataset.firstValue;
-      const operator = calculator.dataset.operator;
-      const secondValue = displayedNum;
-
-      if (firstValue) {
-        if (previousKeyType === "calculate") {
-          firstValue = displayedNum;
-          secondValue = calculator.dataset.modValue;
-        }
-
-        display.textContent = calculate(firstValue, operator, secondValue);
-      }
-
-      calculator.dataset.modValue = secondValue;
-      calculator.dataset.previousKeyType = "calculate";
     }
 
     if (
@@ -198,26 +145,26 @@ keys.addEventListener("click", (e) => {
       action === "multiply" ||
       action === "divide"
     ) {
+      key.classList.add("is-depressed");
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+
+    if (action === "decimal") {
+      display.textContent = displayedNum + ".";
+    }
+
+    if (action === "clear") {
+      console.log("clear key!");
+    }
+
+    if (action === "calculate") {
       const firstValue = calculator.dataset.firstValue;
       const operator = calculator.dataset.operator;
       const secondValue = displayedNum;
 
-      if (
-        firstValue &&
-        operator &&
-        previousKeyType !== "operator" &&
-        previousKeyType !== "calculate"
-      ) {
-        const calcValue = calculate(firstValue, operator, secondValue);
-        display.textContent = calcValue;
-        calculator.dataset.firstValue = calcValue;
-      } else {
-        calculator.dataset.firstValue = displayedNum;
-      }
-
-      key.classList.add("is-depressed");
-      calculator.dataset.previousKeyType = "operator";
-      calculator.dataset.operator = action;
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
   }
 });
