12bd9f7cc1b013bde5b05a6ba6dd6d45d41cc67c
douglasabnovato <douglasabnovato@gmail.com>
2023-04-22 13:26:34 -0300

    vetor multidimensional

--- dev-web-html-css-js-php/tema-5/tarefas/multi_arrays.php
+++ dev-web-html-css-js-php/tema-5/tarefas/multi_arrays.php
@@ -9,11 +9,29 @@
 </head>
 
 <body>
-    <h1>php arrays</h1>
+    <h1>PHP arrays</h1>
+
+    <div>
+        <h2>Módulo 3 | Atividade 1 / 2: vetor multidimensional</h2>
+
+        <ol>
+            <li>Crie mais um array dentro do array $frutas, para armazenar outro tipo de fruta:<br>
+                Frutas tropicais: goiaba, maracujá, banana e manga.
+            </li>
+            <li>Altere o código para que o array seja impresso na tela (campo CONSOLE do emulador).</li>
+            <li>Execute novamente o código e compare o que será impresso no campo CONSOLE do emulador com o conteúdo do botão ”Mostrar solução” abaixo.</li>
+        </ol>
+        <p>
+            Essas estruturas de dados são similares às vistas na disciplina de Matemática, na qual temos também as matrizes, que podem ser chamadas de vetores de vetores.
+        </p>
+        <p>
+            Trazendo esse conceito ao contexto em questão, temos um array de arrays, ou um array dentro de outro array.
+        </p>
+    </div>
 
     <?php
 
-    echo "<p> --- Frutas : vermelhas, cítricas</p>";
+    echo "<p> --- Frutas : vermelhas e cítricas + tropicais</p>";
 
     $frutas = array(
         "vermelhas" => array(
@@ -28,11 +46,19 @@
             "abacaxi",
             "mexerica"
         ),
+        "tropicais" => array(
+            "goiaba",
+            "maracuja",
+            "banana",
+            "manga"
+        ),
     );
 
-    foreach ($frutas as $fruta) {
-        echo $fruta;
-        echo " - \n";
+    foreach ($frutas as $type) {
+        foreach ($type as $fruta => $value) {
+            echo "{$fruta} => {$value}<br>";
+        }
+        echo "<br>";
     }
 
     echo "<p> --- </p>";
